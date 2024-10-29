067dd1795d4269818ab31d9c71d1eb3458cd8159
sidney2k1 <sidsingla02@gmail.com>
2024-10-29 17:16:49 +0530

    main

---
+++ main.py
@@ -0,0 +1,14 @@
+myset={1,2,3}
+print(myset)
+myset={1.0,"hello",(1,2,3)}
+print(myset)
+myset=set([1,2,3,4])
+print(myset)
+myset={1,2,3,4,3,2,1}
+print(myset)
+myset=set([1,2,3,4,5])
+print("Original set:")
+print(myset)
+myset.pop()
+print("New set after removing first element:")
+print(myset)
